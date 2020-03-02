1184a08a786634f2a547bd8012e1797f89974cda
tiago vieira mota <tiago.vieira.mota@gmail.com>
2020-03-02 11:52:19 -0300

    Alteração do height do header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid__container__header border bg-gray-100 border-gray-500 flex justify-center items-center font-semibold text-4xl text-gray-800">
+  <div class="grid__container__header border bg-gray-100 border-gray-500 flex justify-center items-center font-semibold text-4xl text-gray-800 h-full">
     <h1>Home microfront</h1>
   </div>
 </template>
